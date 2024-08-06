e083193ffc177c0339877943793a173e43bd7e50
shubhamkothavade7 <shubhamkothavade7@gmail.com>
2024-08-06 19:16:16 +0530

    Weather app complate project

---
+++ src/components/Weather.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzel_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
+const Weather = () => {
+  const inputRef = useRef();
+
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzel_icon,
+    "04n": drizzel_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+
+  const search = async (city) => {
+    try {
+      if (!city) {
+        alert("Enter City Name");
+      }
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      if (data?.cod == 404) {
+        alert("Invalid City Name");
+      }
+      console.log(data.weather[0].icon);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temprature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    search("London");
+  }, []);
+
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input ref={inputRef} type="text" placeholder="Search" />
+        <img
+          src={search_icon}
+          alt=""
+          onClick={() => search(inputRef.current.value)}
+        />
+      </div>
+      <img src={weatherData.icon} alt="" className="weather-icon" />
+      <p className="temprature">{weatherData.temprature}</p>
+      <p className="location">{weatherData.location}</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>{weatherData.humidity}</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>{weatherData.windSpeed}</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
